0384ca20a12cec4885f68241f9dbed990e15f983
Max Kainov <maxkaynov@gmail.com>
2024-08-04 21:17:57 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='recurCIPY',
+    version='',
+    packages=['recurcipy'],
+    url='',
+    license='',
+    author='Max Kainov',
+    author_email='max.kainov@clickhouse.com',
+    description='Python CI Framework on top of GitHub Actions'
+)
