ce86ad754efb05322bd814d77ab354a07c811da5
Max Kainov <maxkaynov@gmail.com>
2024-08-04 21:52:53 +0200

    First unit test job

--- recurcipy/yaml_generator.py
+++ recurcipy/yaml_generator.py
@@ -22,9 +22,8 @@ class YamlGenerator:
     def hello_world(self):
         with ContextManager.cd():
             Path(Settings.CONFIG_DIRECTORY).mkdir(parents=True, exist_ok=True)
-            with open(f"{Settings.CONFIG_DIRECTORY}/hello_world.py", "w") as f:
-                f.write(HELLO_WORLD_EXAMPLE_PY)
-            Shell.check("git add ./ci/*.py")
+            Shell.check(f"cp {Settings.EXAMPLES_DIRECTORY}/hello_world.py {Settings.CONFIG_DIRECTORY}/")
+            Shell.check(f"git add {Settings.CONFIG_DIRECTORY}/*.py")
         self.generate()
 
     def generate(self):
@@ -126,87 +125,3 @@ if __name__ == '__main__':
         )
     ]
     G.generate()
-
-HELLO_WORLD_EXAMPLE_PY = '''
-
-from typing import List
-
-from recurcipy import Job, Workflow
-from recurcipy.utils import MetaClasses
-
-
-class JobNames(MetaClasses.WithIter):
-    """
-    Inclusive List of Job names
-    """
-    JOB_HELLO_WORLD = "Hello_World"
-    JOB_HELLO_RECURCIPY = "Hello_RecurCIPY"
-    JOB_LINT = "Yaml_Lint"
-
-
-class ArtifactNames(MetaClasses.WithIter):
-    """
-    Predefined names of artifacts
-    """
-    GREET = "greet"
-
-
-class WorkflowNames(MetaClasses.WithIter):
-    """
-    Workflow names
-    """
-    PULL_REQUEST = "Pull Request"
-    MASTER = "Main"
-
-
-workflow_pr = Workflow.Config(
-    name=WorkflowNames.PULL_REQUEST,
-    event=Workflow.Event.PULL_REQUEST,
-    jobs=[
-        Job.Config(
-            name=JobNames.JOB_HELLO_WORLD,
-            command="echo Hello World",
-            provides=[ArtifactNames.GREET],
-            job_requirements=Job.Requirements(python_requirements="requirements.txt")
-        ),
-        Job.Config(
-            name=JobNames.JOB_HELLO_RECURCIPY,
-            command="echo Hello World",
-            requires=[ArtifactNames.GREET],
-            job_requirements=Job.Requirements(python_requirements="requirements.txt")
-        ),
-        Job.Config(
-            name=JobNames.JOB_LINT,
-            command="yamllint . --config-file=.yamllint",
-            requires=[JobNames.JOB_HELLO_RECURCIPY],
-            job_requirements=Job.Requirements(python_requirements="requirements.txt")
-        ),
-    ]
-)
-
-workflow_master = Workflow.Config(
-    name=WorkflowNames.MASTER,
-    event=Workflow.Event.PUSH,
-    jobs=[
-        Job.Config(
-            name=JobNames.JOB_HELLO_WORLD,
-            command="echo Hello Hello World",
-            job_requirements=Job.Requirements(python_requirements="requirements.txt")
-        ),
-        Job.Config(
-            name=JobNames.JOB_LINT,
-            command="yamllint . --config-file=.yamllint",
-            job_requirements=Job.Requirements(python_requirements="requirements.txt")
-        ),
-    ]
-)
-
-"""
-recurCIPY entry-point for generating yaml configs
-each item ends up in workflow yaml file
-"""
-WORKFLOWS = [
-    workflow_pr,
-    workflow_master,
-]  # type: List[Workflow.Config]
-'''
