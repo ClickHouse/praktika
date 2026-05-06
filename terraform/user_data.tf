948f3487db3cbb089d94b68dcd5864dad9f2dc47
Max Kainov <max.kainov@clickhouse.com>
2026-05-06 11:25:52 +0200

    remove tf

--- terraform/user_data.tf
+++
@@ -1,7 +0,0 @@
-data "local_file" "user_data" {
-  filename = "${path.module}/user_data.txt"
-}
-
-data "local_file" "user_data_fixed_size_asg" {
-  filename = "${path.module}/fixed_size_user_data.txt"
-}
